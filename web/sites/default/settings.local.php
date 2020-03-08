c73a63ca8ac9a784dcaf2b016196aa27f418c07a
mo <moshpit2241@gmail.com>
2020-03-08 13:40:43 +0800

    first commit

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Database settings:
+ */
+$databases['default']['default'] = array(
+  'database' => 'mo-dev',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+  'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+);
+
+$settings['hash_salt'] = 'wergf876ghwer9870g6vbw0e8rv7bw0e8r7vbw09e87rbvw8ebrv87wberv';
